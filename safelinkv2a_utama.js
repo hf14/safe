b9faa455988499748f4de8f96e127b3c8da099a7
hf14 <hamzahschneider@gmail.com>
2017-06-27 14:32:02 +0700

    Update safelinkv2a_utama.js

--- safelinkv2a_utama.js
+++ safelinkv2a_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="vps-and-function-of-vps.html"}
 if(x=="2"){xxx="bbm-free-calls-messages.html"}
 if(x=="3"){xxx="maps-navigation-transportation.html"}
-if(x=="4"){xxx="google-assistant.html"}a_to_ve[i].href="https://monitor-link.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="google-assistant.html"}a_to_ve[i].href="http://monitor-link.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
